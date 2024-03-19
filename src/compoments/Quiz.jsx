934aa99718a8bbd31e291c7a39b79a0ef299458c
Deependra <deependra186037@gamil.com>
2024-03-19 13:03:21 +0530

    update

--- src/compoments/Quiz.jsx
+++ src/compoments/Quiz.jsx
@@ -25,33 +25,35 @@ function Quiz() {
 
   return (
     <div>
-      <h1 className=" text-center text-4xl p-3 font-bold">
-        Quiz App
-      </h1>
+      <h1 className=" text-center text-4xl p-3 font-bold">Quiz App</h1>
       <div>
-        {data.map((e, idx) =>
-          count === idx ? (
-            <>
-              <Question
-                key={idx}
-                question={e.question}
-                correct_answer={e.correct_answer}
-                incorrect_answers={e.incorrect_answers}
-                setCount={setCount}
-                count={count}
-                result={result}
-                setResult={setResult}
-              />
-            </>
-          ) : null
+        {count <= 10 &&
+          data.map((e, idx) => {
+            if (count === idx) {
+              return (
+                <>
+                  <Question
+                    key={idx}
+                    question={e.question}
+                    correct_answer={e.correct_answer}
+                    incorrect_answers={e.incorrect_answers}
+                    setCount={setCount}
+                    count={count}
+                    result={result}
+                    setResult={setResult}
+                  />
+                </>
+              );
+            } else {
+              return null;
+            }
+          })}
+        {count >= 10 && (
+          <div className="text-center font-medium">
+            <h2>Congratulations! Quiz Completed!</h2>
+            <p>Your Score: {result} out of 10</p>
+          </div>
         )}
-        {count > 10 && (
-          
-              <div>
-                <h2>Congratulations! Quiz Completed!</h2>
-                <p>Your Score: {result} out of 10</p>
-              </div>
-            )}
       </div>
     </div>
   );
